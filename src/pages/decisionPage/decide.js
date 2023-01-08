31ddd4ea699aac97556ba63c8bbbe7ec006c332b
Azarul Mohammed <theazarul100@gmail.com>
2023-01-08 01:32:54 -0500

    updated decision page

--- src/pages/decisionPage/decide.js
+++ src/pages/decisionPage/decide.js
@@ -1,23 +1,42 @@
 import logo from "../../logo.png"
 import './decide.css';
 import { Link } from "react-router-dom";
+import ButtonComponent from "../../components/ButtonComponent";
 
 
+///Add Page Not Found 
 
 
 function decide() {
     var sessionString = sessionStorage.getItem("theArray");
-    var testss = JSON.parse(sessionString)
-    console.log(testss)
-    let show = testss[Math.floor(Math.random() * testss.length)];
-    console.log(show.name)
+    var choiceArray = JSON.parse(sessionString)
+    let choiceName = choiceArray[Math.floor(Math.random() * choiceArray.length)];
+    // console.log(testss)
+    // let choiceName = choiceArray[Math.floor(Math.random() * choiceArray.length)];
+    // console.log(choiceName.name)
+
+    function chooseChoice() {
+        // choiceName = choiceArray[Math.floor(Math.random() * choiceArray.length)];
+        // console.log(choiceName.name);
+        // return choiceName;
+        window.location.reload(false);
+
+    }
 
     return (
         <>
             <div className="dTop">
-                <Link to ="/"><img src={logo} alt="EyeChoose" style={{ width: 275, height: 175, margin: 0, padding: 0 }} /></Link>
+                <Link to="/"><img src={logo} alt="EyeChoose" style={{ width: 275, height: 175, margin: 0, padding: 0 }} /></Link>
                 <br />
-                <p>{show.name}</p>
+            </div>
+            <div className="dMiddle">
+                <div className="containerText">
+                <div className="textArea"><p className="choosingText">I choose {choiceName.name}</p></div>
+                </div>
+                <div className="buttonsArea">
+                    <ButtonComponent onClick={chooseChoice}>Choose Again</ButtonComponent>
+                    <Link to = "/selection"><ButtonComponent>Add New Choices</ButtonComponent></Link>
+                </div>
             </div>
         </>
     );
--- src/pages/homePage/home.js
+++ src/pages/homePage/home.js
@@ -7,16 +7,16 @@ function Home() {
     return (
         <>
             <div className="Top">
-                <img src={logo} alt="EyeChoose" className = "imageHome" />
+                <img src={logo} alt="EyeChoose" className="imageHome" />
                 <br />
                 <div className="containerSlogan">
-                <div className="sloganArea"><p className='slogan'>Are you indecisive? Ask me instead and I choose what you should do because I can </p></div>
+                    <div className="sloganArea"><p className='slogan'>Are you indecisive? Ask me instead and I choose what you should do because I can </p></div>
                 </div>
             </div>
             <div className='middle'>
-                <Link to = "selection"><ButtonComponent>Start</ButtonComponent></Link>
+                <Link to="selection"><ButtonComponent>Start</ButtonComponent></Link>
                 <br /><br /><br />
-                <Link to = "rules"><ButtonComponent>How To</ButtonComponent></Link>
+                <Link to="rules"><ButtonComponent>How To</ButtonComponent></Link>
             </div>
         </>
     );
