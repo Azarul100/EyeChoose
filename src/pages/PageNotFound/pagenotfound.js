d5f97a06c0648436f8afcbfb132d6c3459f3f11a
Azarul Mohammed <theazarul100@gmail.com>
2023-01-13 01:55:21 -0500

    fixed issues

--- src/pages/PageNotFound/pagenotfound.js
+++ src/pages/PageNotFound/pagenotfound.js
@@ -7,7 +7,7 @@ function PageNotFound() {
     return (
         <>
             <div className="pnfTop">
-                <Link to ="EyeChoose"><img src={logo} alt="EyeChoose" style={{ width: "70vw", height: "40vw", margin: 0, padding: 0 }} /></Link>
+                <Link to ="/"><img src={logo} alt="EyeChoose" style={{ width: "70vw", height: "40vw", margin: 0, padding: 0 }} /></Link>
                 <p className = "pnfText">Page Not Found! Click above to go to the home page</p>
                 <br />
             </div>
